75ba36fa191a95ffc0a45c5624f943afb7a4917a
Invitado L <invitadol@MacBook-Pro-de-Alberto.local>
2020-12-02 23:59:15 -0600

    problems solved

--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var backGorundView: UIView!
+    @IBOutlet weak var rColorSlider: UISlider!
+    @IBOutlet weak var gColorSlider: UISlider!
+    @IBOutlet weak var bColorSlider: UISlider!
+    
+    @IBAction func buttonPresed(_ sender: Any) {
+        
+      
+    }
+    @IBAction func rSliderAction(_ sender: Any) {
+        obtainColor()
+    }
+    @IBAction func gSliderAction(_ sender: Any) {
+        obtainColor()
+    }
+    @IBAction func bSliderAction(_ sender: Any) {
+        obtainColor()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func obtainColor () {
+        let red : Float = rColorSlider.value
+        let green : Float = gColorSlider.value
+        let blue : Float = bColorSlider.value
+        
+        setBackgroundColorFrom(red: red, green: green, blue: blue)
+    }
+    
+    func setBackgroundColorFrom(red: Float, green: Float, blue: Float){
+        let cgfRed: CGFloat = CGFloat(red)
+        let cgfgreen: CGFloat = CGFloat(green)
+        let cgfblue: CGFloat = CGFloat (blue)
+        let color: UIColor = UIColor(displayP3Red: cgfRed, green: cgfgreen, blue: cgfblue, alpha: 1)
+        
+        backGorundView.backgroundColor = color
+        
+    }
+  
 
 
 }
